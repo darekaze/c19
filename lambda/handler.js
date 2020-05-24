70d8230ebca894bb5860f50f93e4cdd0fb4deba4
DaRekaze <darekaze@gmail.com>
2020-05-25 02:04:45 +0800

    add updated at to lambda

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk');
-const mysql = require('mysql2/promise');
-const dbConfig = require('./config/db');
+const AWS = require("aws-sdk");
+const mysql = require("mysql2/promise");
+const dbConfig = require("./config/db");
 
 const s3 = new AWS.S3();
 
@@ -31,8 +31,8 @@ module.exports.generateSummary = async (event, context, callback) => {
   let suspectedCount = 0;
   let confirmedCount = 0;
 
-  records.forEach(record => {
-    const {country, is_suspected, is_confirmed} = record
+  records.forEach((record) => {
+    const { country, is_suspected, is_confirmed } = record;
     countryCount[country] = (countryCount[country] || 0) + 1;
     suspectedCount += is_suspected;
     confirmedCount += is_confirmed;
@@ -40,16 +40,26 @@ module.exports.generateSummary = async (event, context, callback) => {
 
   // Upload to S3
   const destParams = {
-      Bucket: "comp4442a3darwin",
-      Key: "summary.json",
-      Body: JSON.stringify({records, countryCount, suspectedCount, confirmedCount}, null, 2),
-      ContentType: "application/json",
-      CacheControl: "no-cache",
+    Bucket: "comp4442a3darwin",
+    Key: "summary.json",
+    Body: JSON.stringify(
+      {
+        updatedAt: new Date().toLocaleString(),
+        records,
+        countryCount,
+        suspectedCount,
+        confirmedCount,
+      },
+      null,
+      2
+    ),
+    ContentType: "application/json",
+    CacheControl: "no-cache",
   };
   const s3Response = await s3.putObject(destParams).promise();
 
   return {
-    message: 'Function executed successfully!',
+    message: "Function executed successfully!",
     s3Response,
     event,
   };
