7327d9ade10fa72ab0681ad11e3022b14db929df
DaRekaze <darekaze@gmail.com>
2020-05-25 02:54:27 +0800

    finish summary page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Paper, AppBar, Tabs, Tab, Box } from "@material-ui/core";
+import axios from "axios";
 
 import HealthForm from "./healthForm";
 import HealthSummary from "./healthSummary";
@@ -41,6 +42,30 @@ function LinkTab(props) {
 
 export default function App() {
   const [value, setValue] = useState(0);
+  const [records, setRecords] = useState([]);
+  const [countrySum, setCountrySum] = useState({});
+  const [suspected, setSuspected] = useState(0);
+  const [confirmed, setConfirmed] = useState(0);
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://comp4442a3darwin.s3.amazonaws.com/summary.json")
+      .then((res) => {
+        const {
+          records,
+          countryCount,
+          suspectedCount,
+          confirmedCount,
+          updatedAt,
+        } = res.data;
+        setRecords(records);
+        setCountrySum(countryCount);
+        setSuspected(suspectedCount);
+        setConfirmed(confirmedCount);
+        setDate(updatedAt);
+      });
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -64,7 +89,13 @@ export default function App() {
           <HealthForm />
         </TabPanel>
         <TabPanel value={value} index={1}>
-          <HealthSummary />
+          <HealthSummary
+            records={records}
+            countrySum={countrySum}
+            suspected={suspected}
+            confirmed={confirmed}
+            updatedAt={date}
+          />
         </TabPanel>
       </Paper>
     </Container>
